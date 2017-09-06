adf59c62d01958d79af06814899c874e02634279
MargaritaDzhubaba <rikajubaba@gmail.com>
2017-09-06 13:01:49 +0300

    Добавила скрипт и анимацию для появления формы

---
+++ js/script.js
@@ -0,0 +1,54 @@
+var searchForm = document.querySelector(".search-hotel-popup");
+var searchButton = document.querySelector(".search-hotel-btn");
+var lessButton = document.querySelectorAll(".counter .minus");
+var moreButton = document.querySelectorAll(".counter .plus");
+
+var adultInput = document.getElementById("amount-of-adults");
+var childrenInput = document.getElementById("amount-of-children");
+
+var adults = {
+  adults__lessButton: lessButton[0],
+  adults__moreButton: moreButton[0],
+  adultTotal: adultInput.value
+};
+
+var childrens = {
+  childrens__lessButton: lessButton[1],
+  childrens__moreButton: moreButton[1],
+  childrenTotal: childrenInput.value
+}
+
+searchForm.classList.remove("show-popup");
+
+searchButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  searchForm.classList.toggle("show-popup");
+});
+
+adults.adults__lessButton.addEventListener("click", function(e) {
+  if (adults.adultTotal != 0) {
+    adultInput.value = --adults.adultTotal;
+  }
+  console.log(adults.adultTotal);
+  return adults.adultTotal;
+})
+
+adults.adults__moreButton.addEventListener("click", function(e) {
+  adultInput.value = ++adults.adultTotal;
+  console.log(adults.adultTotal);
+  return adults.adultTotal;
+})
+
+childrens.childrens__lessButton.addEventListener("click", function(e) {
+  if (childrens.childrenTotal != 0) {
+    childrenInput.value = --childrens.childrenTotal;
+  }
+  console.log(childrens.childrenTotal);
+  return childrens.childrenTotal;
+})
+
+childrens.childrens__moreButton.addEventListener("click", function(e) {
+  childrenInput.value = ++childrens.childrenTotal;
+  console.log(childrens.childrenTotal);
+  return childrens.childrenTotal;
+})
